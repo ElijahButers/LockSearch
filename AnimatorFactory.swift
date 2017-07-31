6e34b60b527ece3bd92aa74e9953fe3c522681a9
ElijahButers <Elijahbuters@gmail.com>
2017-07-31 19:15:10 +0300

    Added a scaleUp() method to AnimatorFactory class that will create the animations.

--- AnimatorFactory.swift
+++ AnimatorFactory.swift
@@ -12,5 +12,16 @@ class AnimatorFactory {
   
   static func scaleUp(view: UIView) -> UIViewPropertyAnimator {
     
+    let scale = UIViewPropertyAnimator(duration: 0.33, curve: .easeIn)
+    scale.addAnimations {
+      view.alpha = 1.0
+    }
+    scale.addAnimations ({
+      view.transform = CGAffineTransform.identity
+    }, delayFactor: 0.33)
+    scale.addCompletion {_ in
+      print("ready")
+    }
+    return scale
   }
 }
