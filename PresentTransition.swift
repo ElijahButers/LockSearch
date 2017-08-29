65f26af6399bff4247e20e417e375616bd13a9c8
ElijahButers <Elijahbuters@gmail.com>
2017-08-29 20:43:29 +0300

    Added a new swift class to the project named PresentTransition.

---
+++ PresentTransition.swift
@@ -0,0 +1,13 @@
+//
+//  PresentTransition.swift
+//  Widgets
+//
+//  Created by User on 8/29/17.
+//  Copyright © 2017 Underplot ltd. All rights reserved.
+//
+
+import UIKit
+
+class PresentTransition: NSObject {
+  
+}
